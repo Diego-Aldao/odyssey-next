61fdc5bad9271f063dda40d234e86b9b4f73619a
Diego Aldao <diegofrontdev@gmail.com>
2024-11-04 18:00:51 -0300

    ahora el handleClick recibe un id tipo number

--- components/buttons/MainButton.tsx
+++ components/buttons/MainButton.tsx
@@ -3,17 +3,21 @@ import React from "react";
 interface Props {
   children: React.ReactNode;
   customStyles?: string;
-  handleClick?: () => void;
+  handleClick: (id: number) => void;
+  id: number;
 }
 
 export default function MainButton({
   children,
   customStyles,
   handleClick,
+  id,
 }: Props) {
   return (
     <span
-      onClick={handleClick}
+      onClick={() => {
+        handleClick(id);
+      }}
       className={`flex gap-1 items-center cursor-pointer px-4 py-2 border border-main-white/20 rounded-xl hover:border-main-color transition-colors ${customStyles}`}
     >
       {children}
