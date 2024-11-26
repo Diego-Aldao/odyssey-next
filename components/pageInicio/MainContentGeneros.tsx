e56c12593fa9ccb63dac0cb36e77967ad2543903
Diego Aldao <diegofrontdev@gmail.com>
2024-11-26 14:31:09 -0300

    agregado el componente MainButton y la logica para redireccionar a la pagina de busqueda con el genero seleccionado

--- components/pageInicio/MainContentGeneros.tsx
+++ components/pageInicio/MainContentGeneros.tsx
@@ -1,17 +1,29 @@
+"use client";
 import React from "react";
 import { generos } from "@/data/generos";
+import MainButton from "../buttons/MainButton";
+import { useRouter } from "next/navigation";
 
 export default function MainContentGeneros() {
+  const router = useRouter();
+  const handleClick = (id: number) => {
+    router.push(`/busqueda?genres=${id}`);
+  };
+
   return (
     <ul className="flex flex-wrap gap-2 lg:gap-4 2xl:justify-evenly">
       {generos.map((genero) => (
-        <li
-          key={genero.id}
-          className="px-4 py-1 border border-main-white/10 rounded-md hover:border-main-color transition-colors cursor-pointer"
-        >
-          <span className="uppercase text-xs lg:text-sm font-semibold">
-            {genero.nombre}
-          </span>
+        <li key={genero.id} className="">
+          <MainButton
+            id={genero.id}
+            handleClick={() => {
+              handleClick(genero.id);
+            }}
+          >
+            <span className="uppercase text-xs lg:text-sm font-semibold">
+              {genero.nombre}
+            </span>
+          </MainButton>
         </li>
       ))}
     </ul>
