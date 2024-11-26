e5bf30f700a80d0bf64e2223011c65ee5ed9a354
Diego Aldao <diegofrontdev@gmail.com>
2024-11-26 14:14:33 -0300

    corregidos imports y el nombre del componente MainSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import HeroSection from "@/components/containers/page-inicio/hero-section";
-import InicioSection from "@/components/containers/page-inicio/inicio-section";
+import MainSection from "@/components/containers/page-inicio/main-section";
 import MainContentUltimosEpisodios from "@/components/pageInicio/MainContentUltimosEpisodios";
 import MainContentTemporadas from "@/components/pageInicio/MainContentTemporadas";
 import MainContentTops from "@/components/pageInicio/MainContentTops";
-import MainContentPopulares from "@/components/pageInicio/MainContentPopulares";
+import MainContentPopulares from "@/components/pageInicio/SectionPopulares/MainContentPopulares";
 import MainContentGeneros from "@/components/pageInicio/MainContentGeneros";
 import MainContentHero from "@/components/pageInicio/MainContentHero";
 import RandomAnime from "@/components/pageInicio/RandomAnime";
@@ -11,44 +11,43 @@ import MainContentTrailers from "@/components/pageInicio/MainContentTrailers";
 
 export default function Inicio() {
   return (
-    <main className="flex flex-col gap-12 overflow-hidden relative z-[2] px-2 pt-16 md:pt-20 lg:pt-[96px] md:px-8 lg:px-10">
+    <main className="flex flex-col gap-12 overflow-hidden relative px-2 pt-16 md:pt-20 lg:pt-[96px] md:px-8 lg:px-10">
       <RandomAnime />
       <HeroSection>
         <MainContentHero />
       </HeroSection>
       <div className="w-full grid grid-cols-1 gap-12 gap-x-8 2xl:grid-cols-[70%,1fr]">
-        <InicioSection
+        <MainSection
           tituloSeccion="lo mas popular"
           customStyles=" 2xl:col-start-1 2xl:row-start-1"
         >
           <MainContentPopulares />
-        </InicioSection>
-        <InicioSection
+        </MainSection>
+        <MainSection
           tituloSeccion="ultimos trailers"
-          customStyles=" 2xl:col-start-1 2xl:col-span-2 bg-secondary-black/60 rounded-xl"
+          customStyles=" 2xl:col-start-1 2xl:col-span-2"
           nombreDestino="ver mas trailers"
           destino="/trailers"
         >
           <MainContentTrailers />
-        </InicioSection>
-        <InicioSection
+        </MainSection>
+        <MainSection
           tituloSeccion="capitulos recientes"
           customStyles=" 2xl:col-start-1"
           nombreDestino="ver mas episodios"
           destino="/temporadas"
         >
           <MainContentUltimosEpisodios />
-        </InicioSection>
-        <InicioSection
+        </MainSection>
+        <MainSection
           tituloSeccion="anime de temporada"
           customStyles=" 2xl:col-start-1"
           nombreDestino="ver temporadas"
           destino="/temporadas"
         >
           <MainContentTemporadas />
-        </InicioSection>
-
-        <InicioSection
+        </MainSection>
+        <MainSection
           tituloSeccion="top historico"
           customStyles=" 2xl:col-start-2 2xl:row-start-3 2xl:row-span-2"
           nombreDestino="ver mas tops"
@@ -56,14 +55,14 @@ export default function Inicio() {
           asideSection={true}
         >
           <MainContentTops />
-        </InicioSection>
-        <InicioSection
+        </MainSection>
+        <MainSection
           tituloSeccion="generos"
           customStyles="2xl:col-start-2 2xl:row-start-1 2xl:row-span-1"
           asideSection={true}
         >
           <MainContentGeneros />
-        </InicioSection>
+        </MainSection>
       </div>
     </main>
   );
