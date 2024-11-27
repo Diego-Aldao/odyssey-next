b5228445d7c1768adc28c3b9287eeefa4f54d58a
Diego Aldao <diegofrontdev@gmail.com>
2024-11-27 13:30:53 -0300

    ahora el componente puede recibir estilos customs por props

--- components/cards/CardsBaseGrid.tsx
+++ components/cards/CardsBaseGrid.tsx
@@ -3,11 +3,14 @@ import React from "react";
 
 interface Props {
   children: React.ReactNode;
+  customStyles?: string;
 }
 
-export default function CardsBaseGrid({ children }: Props) {
+export default function CardsBaseGrid({ children, customStyles }: Props) {
   return (
-    <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 md:gap-4 xl:gap-6">
+    <div
+      className={`w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 md:gap-4 xl:gap-6 ${customStyles}`}
+    >
       {children}
     </div>
   );
