286026c3f5929a9325d153d02e6e611a74a02cdd
Diego Aldao <diegofrontdev@gmail.com>
2024-11-27 13:18:47 -0300

    reemplazado el main y el grid por componentes de tipo contenedor, añadidos los nuevos componentes de secciones

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,27 @@
-import HeroSection from "@/components/containers/page-inicio/hero-section";
-import MainSection from "@/components/containers/page-inicio/main-section";
-import MainContentUltimosEpisodios from "@/components/pageInicio/MainContentUltimosEpisodios";
-import MainContentTemporadas from "@/components/pageInicio/MainContentTemporadas";
-import MainContentTops from "@/components/pageInicio/MainContentTops";
-import MainContentPopulares from "@/components/pageInicio/SectionPopulares/MainContentPopulares";
-import MainContentGeneros from "@/components/pageInicio/MainContentGeneros";
-import MainContentHero from "@/components/pageInicio/MainContentHero";
 import RandomAnime from "@/components/pageInicio/RandomAnime";
-import MainContentTrailers from "@/components/pageInicio/MainContentTrailers";
+import SectionPopulares from "@/components/pageInicio/SectionPopulares/SectionPopulares";
+import HeroSection from "@/components/pageInicio/HeroSection";
+import SectionTrailers from "@/components/pageInicio/SectionTrailers";
+import SectionEpisodios from "@/components/pageInicio/SectionEpisodios";
+import SectionTemporadas from "@/components/pageInicio/SectionTemporadas";
+import SectionTops from "@/components/pageInicio/SectionTops";
+import SectionGeneros from "@/components/pageInicio/SectionGeneros";
+import InicioMainContainer from "@/components/containers/page-inicio/inicio-main-container";
+import GridMainContainer from "@/components/containers/grid-main-container";
 
 export default function Inicio() {
   return (
-    <main className="flex flex-col gap-12 overflow-hidden relative px-2 pt-16 md:pt-20 lg:pt-[96px] md:px-8 lg:px-10">
+    <InicioMainContainer>
       <RandomAnime />
-      <HeroSection>
-        <MainContentHero />
-      </HeroSection>
-      <div className="w-full grid grid-cols-1 gap-12 gap-x-8 2xl:grid-cols-[70%,1fr]">
-        <MainSection
-          tituloSeccion="lo mas popular"
-          customStyles=" 2xl:col-start-1 2xl:row-start-1"
-        >
-          <MainContentPopulares />
-        </MainSection>
-        <MainSection
-          tituloSeccion="ultimos trailers"
-          customStyles=" 2xl:col-start-1 2xl:col-span-2"
-          nombreDestino="ver mas trailers"
-          destino="/trailers"
-        >
-          <MainContentTrailers />
-        </MainSection>
-        <MainSection
-          tituloSeccion="capitulos recientes"
-          customStyles=" 2xl:col-start-1"
-          nombreDestino="ver mas episodios"
-          destino="/temporadas"
-        >
-          <MainContentUltimosEpisodios />
-        </MainSection>
-        <MainSection
-          tituloSeccion="anime de temporada"
-          customStyles=" 2xl:col-start-1"
-          nombreDestino="ver temporadas"
-          destino="/temporadas"
-        >
-          <MainContentTemporadas />
-        </MainSection>
-        <MainSection
-          tituloSeccion="top historico"
-          customStyles=" 2xl:col-start-2 2xl:row-start-3 2xl:row-span-2"
-          nombreDestino="ver mas tops"
-          destino="/tops"
-          asideSection={true}
-        >
-          <MainContentTops />
-        </MainSection>
-        <MainSection
-          tituloSeccion="generos"
-          customStyles="2xl:col-start-2 2xl:row-start-1 2xl:row-span-1"
-          asideSection={true}
-        >
-          <MainContentGeneros />
-        </MainSection>
-      </div>
-    </main>
+      <HeroSection />
+      <GridMainContainer>
+        <SectionPopulares />
+        <SectionTrailers />
+        <SectionEpisodios />
+        <SectionTemporadas />
+        <SectionTops />
+        <SectionGeneros />
+      </GridMainContainer>
+    </InicioMainContainer>
   );
 }
