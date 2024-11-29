c489026ba6bd70e0293b19c98521b24442edb171
Diego Aldao <diegofrontdev@gmail.com>
2024-11-29 14:54:25 -0300

    añadido el container de tipo grid de la pagina detalle personajes

---
+++ components/containers/page-detalle/personajes-grid-container.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function PersonajesGridContainer({ children }: Props) {
+  return (
+    <div className="contenido mx-auto max-w-[1440px] overflow-hidden px-2 md:px-8 lg:px-10 bg-gradient-to-b from-main-black to-transparent to-25% md:from-transparent w-full md:grid md:grid-cols-[1fr,2.5fr] lg:grid-cols-[250px,1fr] xl:grid-cols-[300px,1fr] md:gap-8 xl:gap-12 2xl:px-0">
+      {children}
+    </div>
+  );
+}
