a14e709556c4ead6022259841b192cde9ec29cec
Diego Aldao <diegofrontdev@gmail.com>
2024-12-03 17:47:54 -0300

    terminada la card y agregada una variacion doble

--- components/cards/SkewCard.tsx
+++ components/cards/SkewCard.tsx
@@ -8,6 +8,73 @@ interface Props {
   subtitulo: string;
   destino: string;
   customImageStyles?: string;
+  customStyles?: string;
+  customShadowStyles?: string;
+}
+
+interface PropsCardDouble {
+  imagenLeft: string;
+  imagenRight: string;
+  nombreTop: string;
+  nombreBottom: string;
+  destinoTop: string;
+  destinoBottom: string;
+}
+
+export function SkewCardDouble({
+  imagenLeft,
+  imagenRight,
+  nombreTop,
+  nombreBottom,
+  destinoTop,
+  destinoBottom,
+}: PropsCardDouble) {
+  return (
+    <div className="-skew-x-[10deg] h-20 xl:h-24  relative w-full flex gap-2  overflow-hidden rounded-md border-2 border-secondary-white/10 group justify-between">
+      <div className="deco invisible opacity-0 group-hover:visible group-hover:opacity-100 w-0 z-[2] h-full bg-main-color group-hover:w-1 absolute top-0 left-0 transition-all"></div>
+
+      <div className="overflow-hidden h-full relative left-0 group-hover:left-1 transition-all">
+        <Image
+          src={imagenLeft}
+          alt=""
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="scale-105"
+        />
+      </div>
+      <div className="deco invisible opacity-0 group-hover:visible group-hover:opacity-100 w-0 z-[2] h-full bg-main-color group-hover:w-1 absolute top-0 right-0 transition-all"></div>
+      <div className=" overflow-hidden h-full relative right-0 group-hover:right-1 transition-all">
+        <Image
+          src={imagenRight}
+          alt=""
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="scale-105"
+        />
+      </div>
+      <div className="info z-10 absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center gap-1">
+        <div className="shadow-left hidden lg:block absolute top-0 left-0 w-1/2 h-full bg-gradient-to-r from-transparent to-main-black to-[55px] xl:to-[65px] group-hover:left-12 transition-all"></div>
+        <div className="shadow-right hidden lg:block absolute top-0 right-0 w-1/2 h-full bg-gradient-to-l from-transparent to-main-black to-[55px] xl:to-[65px] group-hover:right-12 transition-all"></div>
+        <Link
+          href={destinoTop}
+          className="text-xs lg:text-sm line-clamp-1 max-w-[60%] xl:max-w-[65%] 2xl:max-w-[75%] font-medium relative z-[2] hover:text-main-color transition-colors"
+        >
+          {nombreTop}
+        </Link>
+        <span className="text-[10px] xl:text-xs line-clamp-1 text-secondary-white relative z-[2]">
+          como
+        </span>
+        <Link
+          href={destinoBottom}
+          className="text-xs lg:text-sm line-clamp-1 max-w-[60%] xl:max-w-[65%] 2xl:max-w-[75%] font-medium relative z-[2] hover:text-main-color transition-colors"
+        >
+          {nombreBottom}
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default function SkewCard({
@@ -16,13 +83,15 @@ export default function SkewCard({
   subtitulo,
   destino,
   customImageStyles,
+  customShadowStyles,
 }: Props) {
   return (
     <Link
       href={destino}
-      className="-skew-x-[10deg] h-20 xl:h-24  relative w-full flex gap-2  overflow-hidden rounded-md border-2 border-secondary-white/10 group"
+      className="-skew-x-[10deg] h-20 xl:h-24  relative w-full flex gap-2  overflow-hidden rounded-md border-2 border-secondary-white/10 group bg-main-black"
     >
       <div className="deco invisible opacity-0 group-hover:visible group-hover:opacity-100 w-0 h-full bg-main-color group-hover:w-2 absolute top-0 left-0 transition-all"></div>
+
       <span className="imagen h-20 xl:h-24  w-[120px] xl:w-[170px] overflow-hidden absolute top-0 left-0  group-hover:left-2 transition-all">
         <Image
           src={imagen}
@@ -34,11 +103,13 @@ export default function SkewCard({
         />
       </span>
 
-      <span className="info flex flex-col gap-2 w-full bg-gradient-to-r from-transparent group-hover:to-100% transition-all to-main-black to-[120px] lg:to-[120px] xl:to-[170px] items-end z-[2] relative pt-4 pr-4">
-        <span className="font-medium text-sm lg:text-base text-right uppercase line-clamp-1 max-w-[70%]">
+      <span
+        className={`info flex flex-col gap-2 w-full items-end z-[2] relative pt-4 pr-4 after:absolute after:inset-0 after:bg-gradient-to-r after:to-[130px] after:lg:to-[120px] after:xl:to-[160px] after:lg:group-hover:left-20 after:transition-all after:from-transparent after:to-main-black ${customShadowStyles}`}
+      >
+        <span className="font-medium text-sm lg:text-base text-right uppercase line-clamp-1 max-w-[70%] relative z-[2]">
           {titulo}
         </span>
-        <span className="text-xs text-secondary-white font-montserrat capitalize">
+        <span className="text-xs text-secondary-white font-montserrat capitalize relative z-[2]">
           {subtitulo}
         </span>
       </span>
