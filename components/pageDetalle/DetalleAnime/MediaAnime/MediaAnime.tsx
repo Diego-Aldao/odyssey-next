3071913e22103ff1e47a0dc087ac36d1ced33b0b
Diego Aldao <diegofrontdev@gmail.com>
2024-12-02 21:48:23 -0300

    actualizado el nombre del componente SectionEpisodios y quitado el import de VideoCard

--- components/pageDetalle/DetalleAnime/MediaAnime/MediaAnime.tsx
+++ components/pageDetalle/DetalleAnime/MediaAnime/MediaAnime.tsx
@@ -1,10 +1,9 @@
-import VideoCard from "@/components/cards/VideoCard";
 import { BASE_URL_ANIME } from "@/constants";
 import fetchData from "@/services/fetchData";
 import { FetchAnimeMedia } from "@/types/fetchTypes";
 import React from "react";
-import EpisodiosAnime from "./SectionEpisodios";
 import SectionVideos from "./SectionVideos";
+import SectionEpisodios from "./SectionEpisodios";
 
 interface Props {
   id: string;
@@ -18,7 +17,7 @@ export default async function MediaAnime({ id }: Props) {
   return (
     <>
       <SectionVideos promo={data.promo} musicVideos={data.music_videos} />
-      <EpisodiosAnime episodios={data.episodes} id={id} />
+      <SectionEpisodios episodios={data.episodes} id={id} />
     </>
   );
 }
