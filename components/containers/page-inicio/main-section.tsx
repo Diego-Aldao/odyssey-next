746ee4f602bed671c5fc66ae1a4b9b19515c2387
Diego Aldao <diegofrontdev@gmail.com>
2024-12-04 13:47:59 -0300

    modificados los estilos

--- components/containers/page-inicio/main-section.tsx
+++ components/containers/page-inicio/main-section.tsx
@@ -21,11 +21,13 @@ export default function MainSection({
   return (
     <section className={`w-full h-full ${customStyles}`}>
       <div
-        className={`main-container bg-main-black/60 rounded-xl w-full text-main-white flex flex-col gap-8 lg:gap-12 border border-[#4d4d50] ${
-          (!destino || !nombreDestino) && "pb-12 md:pb-14 lg:pb-16"
+        className={`main-container w-full text-main-white flex flex-col gap-8 lg:gap-12 relative after:absolute after:-top-[1px] after:right-0 after:w-[80%] after:h-[1px] after:bg-gradient-to-r after:via-main-color after:from-main-color after:to-main-black  ${
+          !destino || !nombreDestino
+            ? "pb-12 md:pb-14 lg:pb-16"
+            : "before:absolute before:-bottom-[1px] before:left-0 before:w-[81%] before:h-[1px] before:bg-gradient-to-l before:via-main-color before:from-main-color before:to-main-black"
         }`}
       >
-        <header className="h-12 md:h-14 bg-main-black rounded-tl-lg lg:h-16 w-fit pl-2 sm:pl-3 md:pl-4 lg:pl-6 pr-4 sm:pr-6 md:pr-8 lg:pr-10 flex items-center rounded-br-xl border border-[#4d4d50] relative z-[2] border-t-0 border-l-0 -top-[1px] -left-[1px]">
+        <header className="h-12 md:h-14 bg-main-black lg:h-16 w-fit pl-2 pr-4 sm:pr-6 md:pr-8 lg:pr-10 flex items-center relative z-[2] btn-gradient-1 -top-[1px] -left-[1px] border border-main-color border-l-0 border-t-0 rounded-br-lg lg:rounded-br-xl after:absolute after:-bottom-[1px] after:left-0 after:w-[90%] after:h-[1px] after:bg-gradient-to-l after:via-main-black after:from-main-color after:to-main-black">
           <h2
             className={`uppercase text-lg md:text-3xl lg:text-4xl font-black tracking-tight italic ${
               asideSection ? "xl:text-2xl" : "xl:text-5xl"
@@ -33,16 +35,15 @@ export default function MainSection({
           >
             {tituloSeccion}
           </h2>
-          <div className="rounded-border-decoration top-0 -right-10  after:left-0  after:border-[#4d4d50]"></div>
-          <div className="rounded-border-decoration -bottom-10 -left-0 after:left-0 after:border-[#4d4d50]"></div>
+
+          <div className="rounded-border-decoration top-0 -right-10  after:left-0 after:border  after:border-main-color"></div>
         </header>
-        <div className="section-container  sm:px-4 md:px-6 lg:px-8 w-full max-w-[1440px] mx-auto px-2">
+        <div className="section-container  md:px-0 w-full max-w-[1440px] mx-auto px-2">
           {children}
         </div>
         {destino && nombreDestino && (
-          <footer className="h-12 md:h-14 lg:h-16 w-fit bg-main-black pl-2 sm:pl-3 md:pl-4 lg:pl-6 pr-4 sm:pr-6 md:pr-8 lg:pr-10 flex items-center rounded-tl-xl self-end border border-[#4d4d50] border-b-0 border-r-0 relative -bottom-[1px] -right-[1px]">
-            <div className="rounded-border-decoration -top-10 right-0  after:right-0 after:bottom-0  after:border-[#4d4d50]"></div>
-            <div className="rounded-border-decoration -bottom-0 -left-10 after:right-0 after:bottom-0 after:border-[#4d4d50]"></div>
+          <footer className="h-12 md:h-14 lg:h-16 w-fit bg-main-black pl-2 sm:pl-3 md:pl-4 lg:pl-6 pr-4 sm:pr-6 md:pr-8 lg:pr-10 flex items-center rounded-tl-xl self-end border border-main-color border-b-0 border-r-0 relative -bottom-[1px] -right-[1px] after:absolute after:-top-[1px] after:right-0 after:w-[90%] after:h-[1px] after:bg-gradient-to-r after:via-main-black after:from-main-color after:to-main-black">
+            <div className="rounded-border-decoration -bottom-0 -left-10 after:right-0 after:bottom-0 after:border-main-color"></div>
             <Link
               href={destino}
               className={`uppercase md:text-xl lg:text-2xl font-bold tracking-tight italic ${
