84d334481aa7248ddb4e1844a268e2fa5a0c930a
Diego Aldao <diegofrontdev@gmail.com>
2024-12-04 13:47:40 -0300

    añadido un espacio entre el ultimo estilo y la prop customStyles, para que no se superpongan

--- components/containers/page-inicio/inicio-main-container.tsx
+++ components/containers/page-inicio/inicio-main-container.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function InicioMainContainer({ customStyles, children }: Props) {
   return (
     <main
-      className={`flex flex-col gap-12 overflow-hidden relative px-2 pt-16 md:pt-20 lg:pt-[96px] md:px-8 lg:px-10${customStyles}`}
+      className={`flex flex-col gap-12 overflow-hidden relative px-2 pt-16 md:pt-20 lg:pt-[96px] md:px-8 lg:px-10 ${customStyles}`}
     >
       {children}
     </main>
