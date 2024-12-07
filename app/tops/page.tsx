081f34cf418bd3b05aa48c289cec589dc3645ac2
Diego Aldao <diegofrontdev@gmail.com>
2024-12-06 21:29:15 -0300

    corregida la url del fetch para que funcione en todos los casos, y extraido el titulo a un componente reutilizable

--- app/tops/page.tsx
+++ app/tops/page.tsx
@@ -1,3 +1,4 @@
+import MainTitulo from "@/components/MainTitulo";
 import MainContentTops from "@/components/pageTops/MainContentTops";
 import { BASE_URL_TOPS } from "@/constants";
 import fetchData from "@/services/fetchData";
@@ -13,19 +14,18 @@ interface Props {
 export default async function PagineTops({ searchParams }: Props) {
   const stringParams = searchParamsToString(searchParams);
   const { pagination, data } = await fetchData<FetchTops>(
-    `${BASE_URL_TOPS}/anime?limit=24${stringParams}`
+    `${BASE_URL_TOPS}/anime${stringParams || ""}${
+      stringParams ? "&limit=24" : "?limit=24"
+    }`
   );
   return (
     <main className="px-4 md:px-6 lg:px-10 pt-40 lg:pt-48 max-w-[1440px] mx-auto z-[2] relative flex flex-col gap-12 overflow-hidden">
-      <section className="hero flex flex-col gap-4">
-        <h1 className="font-montserrat uppercase text-3xl sm:text-5xl lg:text-6xl xl:text-7xl text-center italic font-black">
-          top anime
-        </h1>
-      </section>
+      <MainTitulo titulo="top anime" />
       <MainContentTops
         hasNextPage={pagination.has_next_page}
         page={pagination.current_page}
         data={data}
+        searchParams={searchParams}
       />
     </main>
   );
