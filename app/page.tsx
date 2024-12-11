4478d19312b50fba0de07ca06b6504afe60a51db
Diego Aldao <diegofrontdev@gmail.com>
2024-12-11 19:52:01 -0300

    extraido todo el contenido estatico de las secciones asyncronas, para agilizar la carga de la ui y mejorar la experiencia de usuario, y agregado un suspense en los componentes asyncronos para mostrar un skeleton mientras se completan

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,11 @@ import SectionTops from "@/components/pageInicio/SectionTops";
 import SectionGeneros from "@/components/pageInicio/SectionGeneros";
 import InicioMainContainer from "@/components/containers/page-inicio/inicio-main-container";
 import GridMainContainer from "@/components/containers/grid-main-container";
+import { Suspense } from "react";
+import MainCardSkeleton from "@/components/skeletons/cards/MainCardSkeleton";
+import CardsBaseGrid from "@/components/cards/CardsBaseGrid";
+import MainSection from "@/components/containers/page-inicio/main-section";
+import VideoCardSkeleton from "@/components/skeletons/cards/VideoCardSkeleton";
 
 export default function Inicio() {
   return (
@@ -16,10 +21,54 @@ export default function Inicio() {
       <HeroSection />
       <GridMainContainer>
         <SectionPopulares />
-        <SectionTrailers />
-        <SectionEpisodios />
-        <SectionTemporadas />
-        <SectionTops />
+        <MainSection
+          tituloSeccion="ultimos trailers"
+          customStyles=" xl:col-start-1 xl:col-span-2"
+          nombreDestino="ver mas trailers"
+          destino="/trailers"
+        >
+          <div className="grid gap-8 lg:gap-4 sm:grid-cols-3 lg:grid-cols-4">
+            <Suspense fallback={<VideoCardSkeleton cantidad={12} />}>
+              <SectionTrailers />
+            </Suspense>
+          </div>
+        </MainSection>
+        <MainSection
+          tituloSeccion="episodios recientes"
+          customStyles=" xl:col-start-1"
+          nombreDestino="ver mas episodios"
+          destino="/temporadas"
+        >
+          <CardsBaseGrid customStyles="xl:grid-cols-4 2xl:grid-cols-6">
+            <Suspense fallback={<MainCardSkeleton cantidad={12} />}>
+              <SectionEpisodios />
+            </Suspense>
+          </CardsBaseGrid>
+        </MainSection>
+        <MainSection
+          tituloSeccion="episodios recientes"
+          customStyles=" xl:col-start-1"
+          nombreDestino="ver mas episodios"
+          destino="/temporadas"
+        >
+          <CardsBaseGrid customStyles="xl:grid-cols-4 2xl:grid-cols-6">
+            <Suspense fallback={<MainCardSkeleton cantidad={12} />}>
+              <SectionTemporadas />
+            </Suspense>
+          </CardsBaseGrid>
+        </MainSection>
+        <MainSection
+          tituloSeccion="top historico"
+          customStyles="xl:col-start-2 xl:row-start-3 xl:row-span-2"
+          nombreDestino="ver mas tops"
+          destino="/tops"
+          asideSection={true}
+        >
+          <div className="w-full px-1 grid gap-4 sm:grid-cols-2 lg:gap-8 xl:grid-cols-1">
+            <SectionTops />
+          </div>
+        </MainSection>
+
         <SectionGeneros />
       </GridMainContainer>
     </InicioMainContainer>
