e1ec0c35624645c94f3660550907ab9e98ffb52c
Diego Aldao <diegofrontdev@gmail.com>
2024-12-11 20:40:27 -0300

    delegada la lógica del fetch de datos y su renderizado al componente MainContentPageTemporada para evitar el bloqueo de la pagina hasta que se complete el fetch

--- app/temporadas/[[...temporada]]/page.tsx
+++ app/temporadas/[[...temporada]]/page.tsx
@@ -1,41 +1,38 @@
 import React from "react";
 import MainContentPageTemporadas from "@/components/pageTemporadas/MainContentPageTemporadas";
-import fetchData from "@/services/fetchData";
-import { FetchTemporadas } from "@/types/fetchTypes";
-import { BASE_URL_TEMPORADAS } from "@/constants";
-import { searchParamsToString } from "@/utils/searchParamsToString";
 import MainTitulo from "@/components/MainTitulo";
 import HeroTemporadas from "@/components/pageTemporadas/HeroTemporadas";
 import PagesNavigationMainContainer from "@/components/containers/pages-navigation/pages-navigation-main-container";
+import FiltrosDesktop from "@/components/pageTemporadas/FiltrosDesktop";
+import FiltrosMobile from "@/components/pageTemporadas/FiltrosMobile";
+import CardsBaseGrid from "@/components/cards/CardsBaseGrid";
 
 interface Props {
   params: { [key: string]: string[] };
   searchParams: { [key: string]: string | undefined };
 }
 
-export default async function Temporadas({ params, searchParams }: Props) {
+export default function Temporadas({ params, searchParams }: Props) {
   const { temporada } = params;
-  const stringParams = searchParamsToString(searchParams);
-  const { data, pagination } = await fetchData<FetchTemporadas>(
-    `${BASE_URL_TEMPORADAS}/${temporada.join("/")}${stringParams || ""}${
-      stringParams ? "&limit=24&sfw" : "?limit=24&sfw"
-    }`
-  );
 
   return (
     <>
       <HeroTemporadas temporada={temporada} />
       <PagesNavigationMainContainer>
         <MainTitulo titulo="anime de temporada" />
-
-        <MainContentPageTemporadas
-          data={data}
-          currentTemporada={temporada}
-          tipoAnime={searchParams?.filter}
-          page={pagination.current_page}
-          hasNextPage={pagination.has_next_page}
-          searchParams={searchParams}
-        />
+        <header className="flex flex-col gap-8 md:pt-4 lg:pt-8 w-full">
+          <FiltrosDesktop
+            currentTemporada={temporada}
+            tipoAnime={searchParams?.filter}
+          />
+          <FiltrosMobile />
+        </header>
+        <CardsBaseGrid>
+          <MainContentPageTemporadas
+            currentTemporada={temporada}
+            searchParams={searchParams}
+          />
+        </CardsBaseGrid>
       </PagesNavigationMainContainer>
     </>
   );
--- components/pageTemporadas/MainContentPageTemporadas.tsx
+++ components/pageTemporadas/MainContentPageTemporadas.tsx
@@ -1,69 +1,67 @@
 "use client";
 import React from "react";
-import CardsBaseGrid from "../cards/CardsBaseGrid";
 import MainCard from "../cards/MainCard/MainCard";
 import { SearchParams } from "@/types/localTypes";
-import { Temporada } from "@/types/fetchTypes";
+import { FetchTemporadas, Temporada } from "@/types/fetchTypes";
 import useNewUrl from "@/hooks/useNewUrl";
 import useInfiniteData from "@/hooks/useInfiniteData";
 import InfiniteScroll from "../InfiniteScroll";
-import FiltrosMobile from "./FiltrosMobile";
-import FiltrosDesktop from "./FiltrosDesktop";
+import { BASE_URL_TEMPORADAS } from "@/constants";
+import { searchParamsToString } from "@/utils/searchParamsToString";
+import useSWR from "swr";
+import fetchData from "@/services/fetchData";
+import MainCardSkeleton from "../skeletons/cards/MainCardSkeleton";
 
 interface Props {
-  data: Temporada[];
   currentTemporada: string[];
-  tipoAnime: string | undefined;
-  page: number;
-  hasNextPage: boolean;
   searchParams: SearchParams;
 }
 
 export default function MainContentPageTemporadas({
-  data,
   currentTemporada,
-  tipoAnime,
-  page,
-  hasNextPage,
   searchParams,
 }: Props) {
-  const { currentData } = useInfiniteData<Temporada>(data, page);
+  const stringParams = searchParamsToString(searchParams);
+
+  const url = `${BASE_URL_TEMPORADAS}/${currentTemporada.join("/")}${
+    stringParams || ""
+  }${stringParams ? "&limit=24&sfw" : "?limit=24&sfw"}`;
+
+  const { data, isLoading } = useSWR(url, fetchData<FetchTemporadas>);
+
+  const page = data?.pagination.current_page;
+
+  const { currentData } = useInfiniteData<Temporada>(data?.data, page);
   const { newUrl } = useNewUrl({ page, searchParams });
 
   return (
     <>
-      <header className="flex flex-col gap-8 md:pt-4 lg:pt-8 col-span-full ml-auto mb-2 md:ml-0 w-full">
-        <FiltrosDesktop
-          currentTemporada={currentTemporada}
-          tipoAnime={tipoAnime}
-        />
-        <FiltrosMobile />
-      </header>
+      {isLoading && !currentData && <MainCardSkeleton cantidad={12} />}
       {currentData && (
-        <CardsBaseGrid customStyles="min-h-screen">
-          <>
-            {currentData.map((anime, i) => (
-              <MainCard
-                nombre={anime.title}
-                tipo="anime"
-                id={anime.mal_id}
-                key={`${anime.mal_id}${i}`}
-                imagen={anime.images.webp.image_url}
-              >
-                <span className="line-clamp-1 text-xs sm:text-sm md:text-xs lg:text-sm font-montserrat font-semibold">
-                  {anime.title}
+        <>
+          {currentData.map((anime, i) => (
+            <MainCard
+              nombre={anime.title}
+              tipo="anime"
+              id={anime.mal_id}
+              key={`${anime.mal_id}${i}`}
+              imagen={anime.images.webp.image_url}
+              destino={`/anime/${anime.mal_id}`}
+              customStyles="2xl:!h-[310px]"
+            >
+              <span className="line-clamp-1 text-xs sm:text-sm md:text-xs lg:text-sm font-montserrat font-semibold">
+                {anime.title}
+              </span>
+              <div className="score flex items-center gap-1">
+                <span className="font-montserrat font-bold text-xs ">
+                  {anime.score || "sin puntuar"}
                 </span>
-                <div className="score flex items-center gap-1">
-                  <span className="font-montserrat font-bold text-xs ">
-                    {anime.score || "sin puntuar"}
-                  </span>
-                  <span className="icon-[solar--star-bold] h-3 w-3 xl:h-4 xl:w-4 text-main-color"></span>
-                </div>
-              </MainCard>
-            ))}
-            {hasNextPage && <InfiniteScroll newUrl={newUrl} />}
-          </>
-        </CardsBaseGrid>
+                <span className="icon-[solar--star-bold] h-3 w-3 xl:h-4 xl:w-4 text-main-color"></span>
+              </div>
+            </MainCard>
+          ))}
+          {data?.pagination.has_next_page && <InfiniteScroll newUrl={newUrl} />}
+        </>
       )}
     </>
   );
