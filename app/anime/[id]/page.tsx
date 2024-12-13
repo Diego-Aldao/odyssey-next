5da174f681be78a8f5af1601c0d7d9ea4c6a041e
Diego Aldao <diegofrontdev@gmail.com>
2024-12-13 20:36:23 -0300

    extraido el hero a un layout, para reutilizarlo en las subpaginas episodios,personajes,etc. y removidos los suspense innecesarios

--- app/anime/[id]/page.tsx
+++ app/anime/[id]/page.tsx
@@ -1,20 +1,12 @@
 import React, { Suspense } from "react";
-import { FetchAnime } from "@/types/fetchTypes";
-import fetchData from "@/services/fetchData";
-import { BASE_URL_ANIME } from "@/constants";
 import MediaAnime from "@/components/pageDetalle/DetalleAnime/MediaAnime/MediaAnime";
 import SectionEstadisticas from "@/components/pageDetalle/DetalleAnime/SectionEstadisticas/SectionEstadisticas";
-import SectionPersonajes from "@/components/pageDetalle/DetalleAnime/SectionPersonajes";
-import SectionRecomendaciones from "@/components/pageDetalle/DetalleAnime/SectionRecomendaciones";
-import SectionReviews from "@/components/pageDetalle/DetalleAnime/SectionReviews";
-import SectionHero from "@/components/pageDetalle/DetalleAnime/SectionHero";
+import SectionPersonajes from "@/components/pageDetalle/DetalleAnime/SectionPersonajes/SectionPersonajes";
+import SectionReviews from "@/components/pageDetalle/DetalleAnime/SectionReviews/SectionReviews";
 import MediaSkeleton from "@/components/skeletons/pageDetalle/MediaSkeleton";
 import EstadisticasSkeleton from "@/components/skeletons/pageDetalle/EstadisticasSkeleton";
-import PersonajesSkeleton from "@/components/skeletons/pageDetalle/PersonajesSkeleton";
-import SectionSkeleton from "@/components/skeletons/SectionSkeleton";
-import ReviewsSkeleton from "@/components/skeletons/pageDetalle/ReviewsSkeleton";
 import AnimeGridContainer from "@/components/containers/page-detalle/anime-grid-container";
-import AnimeMainContainer from "@/components/containers/page-detalle/anime-main-container";
+import SectionRecomendaciones from "@/components/pageDetalle/DetalleAnime/SectionRecomendaciones/SectionRecomendaciones";
 
 interface Props {
   params: { [key: string]: string };
@@ -22,36 +14,17 @@ interface Props {
 
 export default async function PageDetalle({ params }: Props) {
   const { id } = params;
-  const { data } = await fetchData<FetchAnime>(`${BASE_URL_ANIME}/${id}/full`);
   return (
-    <AnimeMainContainer>
-      <SectionHero
-        imagen={data.images.webp.large_image_url}
-        titulo={data.title}
-        sinopsis={data.synopsis}
-        score={data.score}
-        generos={data.genres}
-        año={data.year}
-        estado={data.status}
-        rankPopularidad={data.popularity}
-      />
-      <AnimeGridContainer>
-        <Suspense fallback={<MediaSkeleton />}>
-          <MediaAnime id={id} />
-        </Suspense>
-        <Suspense fallback={<EstadisticasSkeleton />}>
-          <SectionEstadisticas id={id} />
-        </Suspense>
-        <Suspense fallback={<PersonajesSkeleton />}>
-          <SectionPersonajes id={id} />
-        </Suspense>
-        <Suspense fallback={<SectionSkeleton />}>
-          <SectionRecomendaciones id={id} />
-        </Suspense>
-        <Suspense fallback={<ReviewsSkeleton />}>
-          <SectionReviews id={id} />
-        </Suspense>
-      </AnimeGridContainer>
-    </AnimeMainContainer>
+    <AnimeGridContainer>
+      <Suspense fallback={<MediaSkeleton />}>
+        <MediaAnime id={id} />
+      </Suspense>
+      <Suspense fallback={<EstadisticasSkeleton />}>
+        <SectionEstadisticas id={id} />
+      </Suspense>
+      <SectionPersonajes id={id} />
+      <SectionRecomendaciones id={id} />
+      <SectionReviews id={id} />
+    </AnimeGridContainer>
   );
 }
