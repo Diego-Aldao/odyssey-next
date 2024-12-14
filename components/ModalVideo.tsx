6c9de569c0788154aba68e5816d3c340fbd8e3ec
Diego Aldao <diegofrontdev@gmail.com>
2024-12-14 18:03:18 -0300

    añadida la animacion de entrada usando intersection observer y la imagen errorImage

--- components/ModalVideo.tsx
+++ components/ModalVideo.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 interface Props {
   titulo: string;
@@ -13,9 +15,16 @@ export default function ModalVideo({ titulo, url, handleToggler }: Props) {
     handleToggler();
   };
 
+  const [ref, inView] = useInView({
+    threshold: 0,
+  });
+
   return (
     <div
-      className="modal w-full h-screen bg-main-black/95 fixed z-50 left-0 top-0 flex flex-col items-center justify-center gap-4 sm:px-4 px-2 lg:px-0"
+      ref={ref}
+      className={`modal w-full h-screen bg-main-black/95 fixed z-50 left-0 top-0 flex flex-col items-center justify-center gap-4 sm:px-4 px-2 lg:px-0 ${
+        inView ? "opacity-100" : "opacity-0"
+      } transition-opacity`}
       onClick={handleToggler}
     >
       <header className="flex w-full md:w-[720px] lg:w-[920px] items-center justify-between">
@@ -31,7 +40,11 @@ export default function ModalVideo({ titulo, url, handleToggler }: Props) {
           <span className="icon-[solar--close-circle-broken] h-6 w-6 md:h-8 md:w-8 group-hover:text-main-color transition-colors"></span>
         </div>
       </header>
-      <div className=" p-[2px] md:p-1 bg-tertiary-black rounded-xl w-full flex overflow-hidden h-[54vw] md:w-[720px] md:h-[410px] lg:w-[920px] lg:h-[520px]">
+      <div
+        className={`p-[2px] md:p-1 bg-tertiary-black rounded-xl w-full flex overflow-hidden h-[54vw] md:w-[720px] md:h-[410px] lg:w-[920px] lg:h-[520px] ${
+          inView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
+        } transition-transform-opacity delay-300`}
+      >
         <iframe
           src={url}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- components/cards/RankCard/RankCard.tsx
+++ components/cards/RankCard/RankCard.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useInView } from "react-intersection-observer";
+import errorImage from "@/assets/errorImageVideoCard.webp";
 
 interface Props {
   destino: string;
@@ -19,17 +22,28 @@ export default function RankCard({
   destino,
   customStylesImage,
 }: Props) {
+  const [ref, inView] = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
   return (
     <Link
       href={destino}
-      className={`border-main-white/20 h-16 rounded-xl border bg-main-black -skew-x-12 flex items-center overflow-hidden lg:max-w-full relative group transition-colors cursor-pointer after:absolute after:bg-gradient-to-r after:from-transparent after:to-main-black after:via-main-black after:inset-0 group hover:after:left-20 after:transition-all ${customStyles}`}
+      ref={ref}
+      className={`border-main-white/20 h-16 rounded-xl border bg-main-black -skew-x-12 flex items-center overflow-hidden lg:max-w-full relative group transition-colors cursor-pointer after:absolute after:bg-gradient-to-r after:from-transparent after:to-main-black after:via-main-black after:inset-0 group hover:after:left-20 after:transition-all ${customStyles} ${
+        inView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
+      } transition-transform-opacity`}
     >
       <span className="bg-main-black relative z-[2] transition-all flex h-full items-center justify-center w-6 lg:w-8  text-main-white group-hover:w-10  font-montserrat font-bold text-xs lg:text-sm">
         {numeroLista}
       </span>
-      <div className="imagen relative w-[55%] h-full bg-main-black -left-8">
+      <div
+        className={`relative w-[55%] h-full bg-main-black -left-8 ${
+          inView ? "translate-x-0" : "-translate-x-4"
+        } transition-transform`}
+      >
         <Image
-          src={imagen || ""}
+          src={imagen || errorImage}
           alt=""
           width={0}
           height={0}
