ce522630dbcf1cc6f5a40dbc7a405b022a12ce63
Diego Aldao <diegofrontdev@gmail.com>
2024-12-14 14:44:30 -0300

    ahora el fetch lo hace el componente MainContentTrailers, para evitar bloquear la ui estatica mientras se completa el fetch. Añadido un suspense con un skeleton de las cards para mejorar la experiencia de usuario

--- app/trailers/[[...tipo]]/page.tsx
+++ app/trailers/[[...tipo]]/page.tsx
@@ -1,38 +1,25 @@
-import VideoCard from "@/components/cards/VideoCard";
 import PagesNavigationMainContainer from "@/components/containers/pages-navigation/pages-navigation-main-container";
 import MainTitulo from "@/components/MainTitulo";
 import PagesNavigationFiltros from "@/components/PagesNavigationFiltros";
-import { BASE_URL_TRAILERS } from "@/constants";
-import fetchData from "@/services/fetchData";
-import { FetchTrailers } from "@/types/fetchTypes";
-import React from "react";
+import MainContentTrailers from "@/components/pageTrailers/MainContentTrailers";
+import VideoCardSkeleton from "@/components/skeletons/cards/VideoCardSkeleton";
+import React, { Suspense } from "react";
 
 interface Props {
   params: { [key: string]: string[] };
 }
 
-export default async function PageTrailers({ params }: Props) {
+export default function PageTrailers({ params }: Props) {
   const { tipo } = params;
-  const urlTipo = tipo?.includes("popular") ? `/${tipo}` : "";
-  const { data } = await fetchData<FetchTrailers>(
-    `${BASE_URL_TRAILERS}${urlTipo}`
-  );
   return (
     <PagesNavigationMainContainer>
       <MainTitulo titulo="trailers" />
       <section className="flex flex-col gap-8">
         <PagesNavigationFiltros tipo={tipo} baseDestino="trailers" />
         <div className="grid gap-8 xl:gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-          {data.map((trailer) => (
-            <VideoCard
-              id={trailer.entry.mal_id}
-              key={trailer.entry.mal_id}
-              nombreAnime={trailer.entry.title}
-              tipoDeVideo={trailer.title}
-              imagen={trailer.trailer.images.medium_image_url}
-              videoUrl={trailer.trailer.embed_url}
-            />
-          ))}
+          <Suspense fallback={<VideoCardSkeleton cantidad={12} />}>
+            <MainContentTrailers tipo={tipo} />
+          </Suspense>
         </div>
       </section>
     </PagesNavigationMainContainer>
