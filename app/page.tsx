27192bb138cb298b2dc4923d322d23be8894a74c
Diego Aldao <diegofrontdev@gmail.com>
2024-12-18 17:56:50 -0300

    ahora la imagen del hero se renderiza desde la propia pagina para abarcar el total del ancho y no tener conflicto con los paddings del componente HeroSection

--- app/page.tsx
+++ app/page.tsx
@@ -13,64 +13,68 @@ import MainCardSkeleton from "@/components/skeletons/cards/MainCardSkeleton";
 import CardsBaseGrid from "@/components/cards/CardsBaseGrid";
 import MainSection from "@/components/containers/page-inicio/main-section";
 import VideoCardSkeleton from "@/components/skeletons/cards/VideoCardSkeleton";
+import ImageHero from "@/components/pageInicio/SectionHero/ImageHero";
 
 export default function Inicio() {
   return (
-    <InicioMainContainer>
-      <RandomAnime />
-      <HeroSection />
-      <GridMainContainer>
-        <SectionPopulares />
-        <MainSection
-          tituloSeccion="ultimos trailers"
-          customStyles=" xl:col-start-1 xl:col-span-2"
-          nombreDestino="ver mas trailers"
-          destino="/trailers"
-        >
-          <div className="grid gap-8 lg:gap-4 sm:grid-cols-3 lg:grid-cols-4">
-            <Suspense fallback={<VideoCardSkeleton cantidad={12} />}>
-              <SectionTrailers />
-            </Suspense>
-          </div>
-        </MainSection>
-        <MainSection
-          tituloSeccion="episodios recientes"
-          customStyles=" xl:col-start-1"
-          nombreDestino="ver mas episodios"
-          destino="/temporadas"
-        >
-          <CardsBaseGrid customStyles="xl:grid-cols-4 2xl:grid-cols-6">
-            <Suspense fallback={<MainCardSkeleton cantidad={12} />}>
-              <SectionEpisodios />
-            </Suspense>
-          </CardsBaseGrid>
-        </MainSection>
-        <MainSection
-          tituloSeccion="episodios recientes"
-          customStyles=" xl:col-start-1"
-          nombreDestino="ver mas episodios"
-          destino="/temporadas"
-        >
-          <CardsBaseGrid customStyles="xl:grid-cols-4 2xl:grid-cols-6">
-            <Suspense fallback={<MainCardSkeleton cantidad={12} />}>
-              <SectionTemporadas />
-            </Suspense>
-          </CardsBaseGrid>
-        </MainSection>
-        <MainSection
-          tituloSeccion="top historico"
-          customStyles="xl:col-start-2 xl:row-start-3 xl:row-span-2"
-          nombreDestino="ver mas tops"
-          destino="/tops"
-          asideSection={true}
-        >
-          <div className="w-full px-1 grid gap-4 sm:grid-cols-2 lg:gap-8 xl:grid-cols-1">
-            <SectionTops />
-          </div>
-        </MainSection>
+    <>
+      <ImageHero />
+      <InicioMainContainer>
+        <RandomAnime />
+        <HeroSection />
+        <GridMainContainer>
+          <SectionPopulares />
+          <MainSection
+            tituloSeccion="ultimos trailers"
+            customStyles=" xl:col-start-1 xl:col-span-2"
+            nombreDestino="ver mas trailers"
+            destino="/trailers"
+          >
+            <div className="grid gap-8 lg:gap-4 sm:grid-cols-3 lg:grid-cols-4">
+              <Suspense fallback={<VideoCardSkeleton cantidad={12} />}>
+                <SectionTrailers />
+              </Suspense>
+            </div>
+          </MainSection>
+          <MainSection
+            tituloSeccion="episodios recientes"
+            customStyles=" xl:col-start-1"
+            nombreDestino="ver mas episodios"
+            destino="/temporadas"
+          >
+            <CardsBaseGrid customStyles="xl:grid-cols-4 2xl:grid-cols-6">
+              <Suspense fallback={<MainCardSkeleton cantidad={12} />}>
+                <SectionEpisodios />
+              </Suspense>
+            </CardsBaseGrid>
+          </MainSection>
+          <MainSection
+            tituloSeccion="episodios recientes"
+            customStyles=" xl:col-start-1"
+            nombreDestino="ver mas episodios"
+            destino="/temporadas"
+          >
+            <CardsBaseGrid customStyles="xl:grid-cols-4 2xl:grid-cols-6">
+              <Suspense fallback={<MainCardSkeleton cantidad={12} />}>
+                <SectionTemporadas />
+              </Suspense>
+            </CardsBaseGrid>
+          </MainSection>
+          <MainSection
+            tituloSeccion="top historico"
+            customStyles="xl:col-start-2 xl:row-start-3 xl:row-span-2"
+            nombreDestino="ver mas tops"
+            destino="/tops"
+            asideSection={true}
+          >
+            <div className="w-full px-1 grid gap-4 sm:grid-cols-2 lg:gap-8 xl:grid-cols-1">
+              <SectionTops />
+            </div>
+          </MainSection>
 
-        <SectionGeneros />
-      </GridMainContainer>
-    </InicioMainContainer>
+          <SectionGeneros />
+        </GridMainContainer>
+      </InicioMainContainer>
+    </>
   );
 }
