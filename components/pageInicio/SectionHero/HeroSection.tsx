c5f8aca3d1b4074f937998ac90bfe384d630e00c
Diego Aldao <diegofrontdev@gmail.com>
2024-12-18 23:52:11 -0300

    corregidas las clases y eliminado el componente ImageHero, que ahora se renderiza directemente desde PageInicio

--- components/pageInicio/SectionHero/HeroSection.tsx
+++ components/pageInicio/SectionHero/HeroSection.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ImageHero from "./ImageHero";
 import TituloHero from "./TituloHero";
 
 export default function HeroSection() {
   return (
-    <section className="flex flex-col sm:gap-y-12  w-full max-w-[1440px] mx-auto justify-end relative">
-      <ImageHero />
+    <section className="flex flex-col sm:gap-y-12 h-[450px] lg:h-[520px] xl:h-[600px] w-full max-w-[1440px] mx-auto justify-end relative">
       <TituloHero />
     </section>
   );
