8c9114a9e6db064d18014489dd9eca049feaeb5f
Diego Aldao <diegofrontdev@gmail.com>
2024-12-18 23:48:39 -0300

    añadida la propiedad unoptimized al componente Image para no saturar el maximo mensual de optimizaciones de imagenes de vercel, añadida la imagen errorImage para mostrar si la imagen no carga o no existe, corregidos los estilos, añadido el componente Link y un condicional para mostrar el componente NoData en caso de que el length de la data sea 0

--- components/pageDetalle/DetallePersonaje/AsideSubInfo.tsx
+++ components/pageDetalle/DetallePersonaje/AsideSubInfo.tsx
@@ -1,6 +1,9 @@
+import NoData from "@/components/errors/NoData";
 import { AnimeElement } from "@/types/fetchTypes";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import errorImage from "@/assets/errorImageMainCard.webp";
 
 interface Props {
   imagen: string;
@@ -10,39 +13,54 @@ interface Props {
 export default function AsideSubInfo({ imagen, participaciones }: Props) {
   return (
     <aside className="aside hidden md:flex flex-col gap-4">
-      <div className="imagen overflow-hidden rounded-2xl border-2 border-secondary-white/20">
+      <div className="imagen overflow-hidden min-h-[295px] lg:min-h-[385px] xl:min-h-[464px] rounded-2xl border-2 bg-secondary-black border-secondary-white/20">
         <Image
-          src={imagen}
+          src={imagen || errorImage}
           alt=""
           width={0}
           height={0}
           sizes="100vw"
           className="scale-105"
+          unoptimized
         />
       </div>
       <div className="animes flex flex-col gap-2">
         <h2 className="uppercase text-lg lg:text-xl xl:text-2xl font-montserrat font-black italic">
           participaciones
         </h2>
-        {participaciones.slice(0, 24).map((item) => (
-          <div key={item.anime.mal_id} className="flex items-start gap-2">
-            <div className="imagen min-w-[50px] w-[50px] h-[70px] rounded-md overflow-hidden">
-              <Image
-                src={item.anime.images.webp.small_image_url}
-                alt=""
-                width={0}
-                height={0}
-                sizes="100vw"
-              />
-            </div>
-            <div className="flex-col flex gap-2">
-              <span className="text-sm font-medium line-clamp-2">
-                {item.anime.title}
-              </span>
-              <span className="text-xs text-secondary-white">{item.role}</span>
-            </div>
-          </div>
-        ))}
+        {participaciones && participaciones.length >= 1 && (
+          <>
+            {participaciones.slice(0, 24).map((item, i) => (
+              <div
+                key={`${item.anime.mal_id}${i}`}
+                className="flex items-start gap-2"
+              >
+                <div className="imagen min-w-[50px] w-[50px] h-[70px] rounded-md overflow-hidden">
+                  <Image
+                    src={item.anime.images.webp.small_image_url}
+                    alt=""
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    unoptimized
+                  />
+                </div>
+                <div className="flex-col flex gap-2">
+                  <Link
+                    href={`/anime/${item.anime.mal_id}`}
+                    className="text-sm font-medium line-clamp-2 hover:text-main-color transition-colors"
+                  >
+                    {item.anime.title}
+                  </Link>
+                  <span className="text-xs text-secondary-white">
+                    {item.role}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+        {participaciones && participaciones.length === 0 && <NoData />}
       </div>
     </aside>
   );
