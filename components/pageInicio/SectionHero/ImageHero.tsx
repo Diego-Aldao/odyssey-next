b021ae42bf77947c2a72eeadf6e929f77b7257ad
Diego Aldao <diegofrontdev@gmail.com>
2024-12-19 22:31:14 -0300

    quitado el triggerOnce de la animacion de entrada, añadida una version mobile de la imagen y corregidos los estilos

--- components/pageInicio/SectionHero/ImageHero.tsx
+++ components/pageInicio/SectionHero/ImageHero.tsx
@@ -2,31 +2,46 @@
 import Image from "next/image";
 import React from "react";
 import { useInView } from "react-intersection-observer";
-import imagen from "@/assets/mainPage/imagenHero.webp";
+import imagen from "@/assets/mainPage/HomeHero.webp";
+import imagenMobile from "@/assets/mainPage/HomeHeroMobile.webp";
 
 export default function ImageHero() {
   const { ref, inView } = useInView({
     threshold: 0.4,
-    triggerOnce: true,
   });
   return (
-    <div className="w-[150%] max-w-[500px] -left-20 relative sm:w-[700px] sm:max-w-full sm:-left-0 md:left-0 md:w-[800px] md:-mt-12 scale-y-95 lg:w-full ml-auto   scale-x-[-1] lg:h-[650px] xl:h-[800px] ">
-      <Image
-        src={imagen}
-        alt=""
-        width={0}
-        height={0}
-        sizes="100vw"
-        className={`!object-contain ${
-          inView
-            ? "lg:opacity-100 lg:translate-y-0"
-            : "lg:opacity-0 lg:translate-y-2"
-        } lg:transition-transform-opacity`}
-        placeholder="blur"
-        blurDataURL={imagen.blurDataURL}
-        priority={true}
-        ref={ref}
-      />
+    <div
+      ref={ref}
+      className={`absolute h-[550px] p-2 bg-main-black lg:h-[650px] xl:h-[800px] 2xl:h-[900px] top-0 left-0 w-full after:absolute after:inset-0 after:bg-gradient-to-t after:from-main-black after:via-transparent after:to-main-black  scale-x-[-1] after:from-5% xl:after:from-[10%] 2xl:after:from-20% transition-transform-opacity ${
+        inView
+          ? "lg:opacity-100 lg:translate-y-0"
+          : "lg:opacity-0 lg:translate-y-4"
+      }`}
+    >
+      <div className="imagen w-full h-full rounded-xl bg-secondary-black overflow-hidden">
+        <Image
+          src={imagenMobile}
+          placeholder="blur"
+          blurDataURL={imagenMobile.blurDataURL}
+          alt=""
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="object-[35%,0px] mix-blend-difference md:hidden"
+          priority={true}
+        />
+        <Image
+          src={imagen}
+          placeholder="blur"
+          blurDataURL={imagen.blurDataURL}
+          alt=""
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="!object-cover object-[35%,0px] mix-blend-difference md:inline-block hidden"
+          priority={true}
+        />
+      </div>
     </div>
   );
 }
