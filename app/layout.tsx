2e7d0827b1a0491f07380d6e45130869bb38da7b
Diego Aldao <diegofrontdev@gmail.com>
2024-12-22 22:47:25 -0300

    añadida la metadata del sitio y eliminado el div que contenia el background

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,27 @@ const raleway = Raleway({
 });
 
 export const metadata: Metadata = {
-  title: "Odyssey: Comunidad Anime",
+  title: "Odyssey — Comunidad Anime",
+  description:
+    "Odyssey es el mejor sitio web para la comunidad de anime en español, con informacion sobre animes, temporadas, personajes, actores de voz, y mucho mas.",
+  keywords:
+    "anime, Odyssey, seguimiento, calificaciones, reseñas, series, películas, recomendaciones, actores de voz, personajes de anime.",
+  authors: [{ name: "Diego Aldao", url: "https://diegoaldao.vercel.app/" }],
+  openGraph: {
+    title: "Odyssey - Comunidad Anime",
+    description:
+      "Odyssey es tu red social y base de datos para animes y mangas, con rankings, reseñas y más.",
+    url: "https://odysseyanime.vercel.app/",
+    type: "website",
+    locale: "es_ES",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Odyssey - Comunidad Anime",
+    description:
+      "Descubre y organiza tus animes y mangas favoritos en Odyssey. Conoce lo mejor del anime.",
+  },
+  robots: "index, follow",
 };
 
 export default function RootLayout({
@@ -33,7 +53,6 @@ export default function RootLayout({
       <body
         className={`${raleway.variable} ${montserrat.variable} relative antialiased bg-main-black w-full min-h-screen `}
       >
-        <div className="background w-full h-full bg-main-black opacity-15 absolute bg-contain"></div>
         <Providers>
           <Nav />
           {children}
