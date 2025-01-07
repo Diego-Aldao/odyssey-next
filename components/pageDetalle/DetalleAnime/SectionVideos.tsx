4408ef4e2da647d23c43f74ab8a964ff77539c7c
Diego Aldao <diegofrontdev@gmail.com>
2025-01-07 12:54:45 -0300

    ahora es el propio componente el que realiza el fetch, quitadas las props innecesarias y corregidos los estilos

--- components/pageDetalle/DetalleAnime/SectionVideos.tsx
+++ components/pageDetalle/DetalleAnime/SectionVideos.tsx
@@ -1,22 +1,26 @@
 import VideoCard from "@/components/cards/VideoCard";
-import { MusicVideo, Promo } from "@/types/fetchTypes";
+import { BASE_URL_ANIME } from "@/constants";
+import fetchData from "@/services/fetchData";
+import { FetchAnimeMedia } from "@/types/fetchTypes";
 import React from "react";
 
 interface Props {
-  promo: Promo[];
-  musicVideos: MusicVideo[];
+  id: string;
 }
 
-export default function SectionVideos({ promo, musicVideos }: Props) {
-  const mainTrailers = promo.slice(0, 12);
-  const mainExtra = musicVideos.slice(0, 12);
+export default async function SectionVideos({ id }: Props) {
+  const { data } = await fetchData<FetchAnimeMedia>(
+    `${BASE_URL_ANIME}/${id}/videos`
+  );
+  const mainTrailers = data.promo.slice(0, 12);
+  const mainExtra = data.music_videos.slice(0, 12);
   const longitudMedia = mainTrailers.length + mainExtra.length;
   return (
     <>
       {longitudMedia >= 1 && (
-        <section className="xl:col-span-full xl:col-start-1 xl:row-start-1 w-[100%] hidden md:block max-w-[1440px] mx-auto overflow-hidden">
+        <section className="w-[100%] hidden md:block max-w-[1440px] mx-auto overflow-hidden">
           <div
-            className={`contenido flex gap-4 max-w-7xl  w-full custom-scroll ${
+            className={`contenido flex gap-4 max-w-7xl overflow-y-hidden w-full custom-scroll ${
               longitudMedia >= 4 && longitudMedia < 6
                 ? "md:overflow-x-scroll xl:overflow-x-hidden pb-4"
                 : longitudMedia >= 6
@@ -40,7 +44,6 @@ export default function SectionVideos({ promo, musicVideos }: Props) {
                 key={`${item.video.youtube_id}${i}`}
                 id={item.video.youtube_id}
                 imagen={item.video.images.medium_image_url}
-                nombreAnime={""}
                 videoUrl={item.video.embed_url}
                 tipoDeVideo={item.title}
                 customStyles="md:max-w-[180px] md:min-w-[180px] lg:max-w-[230px] lg:min-w-[230px] w-full"
